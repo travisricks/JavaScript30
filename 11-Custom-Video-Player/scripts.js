791b8bd4843aa0c238b7fd285b3019633700ca07
Travis Ricks <travis@travisricks.com>
2020-04-16 16:44:36 +0900

    define variables, create togglePlay func

---
+++ 11-Custom-Video-Player/scripts.js
@@ -0,0 +1,16 @@
+/* Get our elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelector("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+/* Build out functions */
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+
+/* Connect event listeners */
+video.addEventListener("click", togglePlay);
