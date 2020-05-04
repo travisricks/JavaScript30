c7f7c79ae7b07f9028085d4072bbcedae7b54419
Travis Ricks <travis@travisricks.com>
2020-05-04 20:06:48 +0900

    finish 19 - fun with webcam

--- 19-Webcam-Fun/scripts.js
+++ 19-Webcam-Fun/scripts.js
@@ -3,3 +3,118 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => {
+      console.log(`SADNESS`, err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth,
+    height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // alter them
+    if (isRedEffect) {
+      pixels = redEffect(pixels);
+      ctx.globalAlpha = 1; // reset from rgbSplit
+    }
+    if (isRgbEffect) {
+      pixels = rgbSplit(pixels);
+      ctx.globalAlpha = 0.1; // stacks images on top of each other with transparency
+    }
+
+    // pixels = greenScreen(pixels);
+
+    // put the pixels back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+let isRedEffect = false;
+function toggleRedEffect() {
+  isRedEffect = !isRedEffect;
+  isRgbEffect = false;
+}
+let isRgbEffect = false;
+function toggleRgbEffect() {
+  isRgbEffect = !isRgbEffect;
+  isRedEffect = false;
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "cool-pic");
+  link.innerHTML = `<img src="${data}" alt="Cool Pic" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // red
+    pixels.data[i + 500] = pixels.data[i + 1]; // green
+    pixels.data[i - 550] = pixels.data[i + 2]; // blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
+getVideo();
+// paintToCanvas();
+
+video.addEventListener("canplay", paintToCanvas);
